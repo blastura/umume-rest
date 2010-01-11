bc502b7b08d4b1406dfbe6d9c0f4e8887a6c8a39
dit06ajn <dit06ajn@c009bb81-029e-c990-cb2b-ce2c03581983>
2010-01-11 22:04:23 +0000

    CAS validates ticket in test

---
+++ src/main/java/se/umu/cs/umume/util/CASUtils.java
@@ -0,0 +1,45 @@
+package se.umu.cs.umume.util;
+
+import org.jasig.cas.client.authentication.AttributePrincipal;
+import org.jasig.cas.client.validation.Assertion;
+import org.jasig.cas.client.validation.Cas20ProxyTicketValidator;
+import org.jasig.cas.client.validation.Saml11TicketValidator;
+import org.jasig.cas.client.validation.TicketValidationException;
+
+public class CASUtils {
+
+    public static final boolean validateTicketSAML(String ticket) {
+        Saml11TicketValidator sv = new Saml11TicketValidator("https://cas.umu.se/");
+        String legacyServerServiceUrl = "http://localhost:8080/UmuMeREST/users/aonjon04";
+        try {
+            AttributePrincipal principal = null;
+            Assertion a = sv.validate(ticket, legacyServerServiceUrl);
+            principal = a.getPrincipal();
+            System.out.println("user name:" + principal.getName());
+        } catch (TicketValidationException e) {
+            e.printStackTrace(); // bad style, but only for demonstration purpose.
+        }
+        return false;
+    }
+
+
+
+    public static final boolean validateTicket(String ticket) {
+        AttributePrincipal principal = null;
+        String casServerUrl = "https://cas.umu.se/";
+        Cas20ProxyTicketValidator sv = new Cas20ProxyTicketValidator(casServerUrl);
+        sv.setAcceptAnyProxy(true);
+        try {
+            // there is no need, that the legacy application is accessible
+            // through this URL. But for validation purpose, even a non-web-app
+            // needs a valid looking URL as identifier.
+            String legacyServerServiceUrl = "http://localhost:8080/UmuMeREST/users/aonjon04";
+            Assertion a = sv.validate(ticket, legacyServerServiceUrl);
+            principal = a.getPrincipal();
+            System.out.println("user name:" + principal.getName());
+        } catch (TicketValidationException e) {
+            e.printStackTrace(); // bad style, but only for demonstration purpose.
+        }
+        return principal != null;
+    }
+}
---
+++ src/test/java/se/umu/cs/umume/util/CASUtilsTest.java
@@ -0,0 +1,10 @@
+package se.umu.cs.umume.util;
+
+import org.junit.Test;
+
+public class CASUtilsTest {
+    @Test
+    public void testValidate() {
+        CASUtils.validateTicket("ST-534199-A6ufrcdfE0o18VYZdgVy");
+    }
+}
