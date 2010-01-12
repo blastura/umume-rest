e20c0496d1726cad779ef8c1332dc67779cf212b
dit06ajn <dit06ajn@c009bb81-029e-c990-cb2b-ce2c03581983>
2010-01-12 08:24:41 +0000

    test cas

--- src/test/java/se/umu/cs/umume/util/CASUtilsTest.java
+++ src/test/java/se/umu/cs/umume/util/CASUtilsTest.java
@@ -5,6 +5,6 @@ import org.junit.Test;
 public class CASUtilsTest {
     @Test
     public void testValidate() {
-        CASUtils.validateTicket("ST-534199-A6ufrcdfE0o18VYZdgVy");
+        CASUtils.validateTicket("ST-544717-SG19b2m7n9mWOGZdF3zd");
     }
 }
