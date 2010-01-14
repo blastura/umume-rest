7dde40b43a1f21e7a46ebaac74679f22bec52e2a
dit06ajn <dit06ajn@c009bb81-029e-c990-cb2b-ce2c03581983>
2010-01-14 09:34:17 +0000

    twitter fix

--- src/main/java/se/umu/cs/umume/util/TwitterUtils.java
+++ src/main/java/se/umu/cs/umume/util/TwitterUtils.java
@@ -4,14 +4,18 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import se.umu.cs.umume.PersonBean;
+import se.umu.cs.umume.rest.resources.SearchResource;
 import twitter4j.Paging;
 import twitter4j.Status;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 
 public class TwitterUtils {
-    /*sug*/
+    private static final Logger logger = LoggerFactory.getLogger(TwitterUtils.class);
     public static List<PersonBean> getTweets(List<PersonBean> persons) {
         for (PersonBean person : persons) {
             String userName = person.getTwitterName();
@@ -20,17 +24,18 @@ public class TwitterUtils {
                 List<Status> statuses = new ArrayList<Status>();
                 try {
                     statuses = twitter.getUserTimeline(userName, new Paging(1));
-                } catch (TwitterException e) {
-                    e.printStackTrace();
-                }
 
-                // Convert from Status to String
-                List<String> stringStatuses = new ArrayList<String>();
-                for(Status status : statuses) {
-                    Date hej = status.getCreatedAt();
-                    stringStatuses.add(status.getText());
+
+                    // Convert from Status to String
+                    List<String> stringStatuses = new ArrayList<String>();
+                    for(Status status : statuses) {
+                        Date hej = status.getCreatedAt();
+                        stringStatuses.add(status.getText());
+                    }
+                    person.setTweets(stringStatuses);
+                } catch (TwitterException e) {
+                    logger.info("Twitter for '{}': {}", userName, e.getMessage());
                 }
-                person.setTweets(stringStatuses);
             }
         }
         return persons;
